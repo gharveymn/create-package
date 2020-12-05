972bdd6057f12aca9cc693c7d4d07c23b5f5af32
Gene Harvey <gharveymn@gmail.com>
2020-12-05 16:52:23 -0600

    update tests

--- test/executable/source/src/main.cpp
+++ test/executable/source/src/main.cpp
@@ -7,7 +7,7 @@
  * of the MIT license. See the LICENSE file for details.
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
  
-#include <test-shared.hpp>
+#include <gharveymn/test-header-only.hpp>
 
 int main (void)
 {
